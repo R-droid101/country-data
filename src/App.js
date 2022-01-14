47f06ca41a902cc653c1a69bfeecdf843ede37f0
Rdroid-101 <raggav.subramani@gmail.com>
2022-01-14 11:57:24 +0530

    feat: search bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Filter from './components/Filter'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Filter from "./components/Filter";
 
 function App() {
-  const [countries, setCountries] = useState([])
-  const [displayDetails, setDisplayDetails] = useState([])
+  const [countries, setCountries] = useState([]);
+  const [displayDetails, setDisplayDetails] = useState("");
 
   useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all")
-    .then(response => {
-      console.log(response.data)
-      setCountries(response.data)
-    })
-  }, [])
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      // console.log(response.data)
+      setCountries(response.data);
+    });
+  }, []);
 
-  const changeDetails = e => setDisplayDetails(e.target.value)
+  const changeDetails = (e) => setDisplayDetails(e.target.value);
 
   return (
     <>
-    <div>filter countries<input value={displayDetails} onChange={changeDetails}/></div>
-    {/* {JSON.stringify(countries[0].name.common)} */}
-      {/* {countries.map(country => <div>{country.name.common} - {country.languages.ell}</div>)} */}
-      {/* {countries.map(country => <div key={country.name.official}>{country.name.common}</div>)} */}
-      <Filter countries={countries}/>
+      <div>
+        filter countries
+        <input value={displayDetails} onChange={changeDetails} />
+      </div>
+      <Filter countries={countries} displayDetails={displayDetails} />
     </>
   );
 }
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,10 +1,50 @@
 function Filter(props) {
+  const displayCountries = props.countries.filter((country) =>
+    country.name.common
+      .toLowerCase()
+      .includes(props.displayDetails.toLowerCase())
+  );
+
+  if (displayCountries.length > 10) {
+    return <div>Too many matches, specify another filter</div>;
+  }
+
+  else if (displayCountries.length <= 10 && displayCountries.length > 1) {
     return (
+      <>
         <div>
-            {props.countries.map(country => <div key={country.name.official}>{country.name.common}</div>)}
+          {displayCountries.map((country) => (
+            <div key={country.name.official}>{country.name.common}</div>
+          ))}
         </div>
+      </>
+    );
+  }
+
+  else {
+    return (
+      <>
+        <div>
+          {displayCountries.map(country => {
+            return (
+              <div key={country.name.official}>
+                <h1>{country.name.common}</h1>
+                <p>
+                  capital {country.capital}<br />
+                  populaton {country.population}
+                </p>
+                <h2>languages</h2>
+                <ul>
+                  {Object.keys(country.languages).map((key, value) => <li key={key}>{country.languages[key]}</li>)}
+                </ul>
+                <img src={country.flags.png} alt="Flag" />
+              </div>
+            )
+          })}
+        </div>
+      </>
     )
+  }
 }
 
-export default Filter
-
+export default Filter;
